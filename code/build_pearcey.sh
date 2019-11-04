332022f3aa1e437102bf7def915e868ecc621264
WilkAndy <andrew.wilkins@csiro.au>
2019-11-05 10:03:13 +1100

    build_pearcey.sh added - builds cython stuff on pearcey architecture
    Fixes #2

---
+++ code/build_pearcey.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cythonize -3 -a *.pyx
+
+gcc_flags="-shared -fno-strict-aliasing -Wsign-compare -Wunreachable-code -DNDEBUG -g -O3 -Wall -I/apps/python/3.7.2/include/python3.7m -fPIC"
+mpicc $gcc_flags Grid.c -o Grid.so
+
