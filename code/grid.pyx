ed8fdfec1972250cfb836d00b20b33bed671ce43
WilkAndy <andrew.wilkins@csiro.au>
2019-11-05 13:36:38 +1100

    Grid now has notion of active and inactive cells.  Fixes #1

--- code/grid.pyx
+++ code/grid.pyx
@@ -23,6 +23,12 @@ cdef class Grid:
     # number of active cells
     cpdef unsigned num_active_cells
 
+    # global_index[i] = global index of the active cell index.  Here i ranges from 0 to num_active_cells - 1, and the global_index values will be between 0 and num_cells - 1
+    cpdef array.array global_index
+
+    # active[i] = active cell index of the global cell index.  Here i ranges from 0 to num_cells - 1, and active_index values will range from 0 to num_active_cells - 1
+    cpdef array.array active_index
+
     # connectivity information: connect_from[i] is a cell number that connects to cell number connect_to[i].  These arrays contain only active cells
     cpdef array.array connect_from
     cpdef array.array connect_to
@@ -45,7 +51,110 @@ cdef class Grid:
 
         # initialise activity information to "all active"
         self.active = array.array('I', [1] * self.num_cells)
-        self.num_active_cells = nx * ny
+
+        # build active and adjacency information
+        self.computeNumActive()
+        self.buildAdjacency()
+
+    cdef unsigned internal_global_index(self, unsigned x_ind, unsigned y_ind):
+        """provides the index used in this class, given x and y cell number.
+        Note that:
+        (x_ind, y_ind) = 0 corresponds to the lower left-hand cell, which is internal_global_index = 0
+        (x_ind, y_ind) = (nx - 1, 0) corresponds to the lower right-hand cell, which is internal_global_index = nx - 1
+        (x_ind, y_ind) = (0, ny - 1) corresponds to the upper left-hand cell, which is internal_global_index = (ny - 1) * nx
+        (x_ind, y_ind) = (nx - 1, ny - 1) corresponds to the upper right-hand cell, which is internal_global_index = (ny - 1) * nx + nx - 1
+        """
+        return y_ind * self.nx + x_ind
+
+    cpdef setActiveAndInactive(self, filename):
+        """Parses filename to determine the active and inactive cells.
+        Updates connectivity information, so getConnectionsFrom, etc, only returns active cells.
+        filename must have the following format:
+        - any empty lines are ignored
+        - Data must be preceded by a line of the form '#xmin=a,ymin=b,cell_size=c,nx=d,ny=e' where a,b,c,d,e must match that given in the constructor
+             This allows for error checking
+        - Any line that starts with # is ignored (except for the '#xmin...' line)
+        - Data is CSV form.
+            The first row corresponds to the ymin set of cells
+            The second row corresponds to the ymin+cell_size set of cells
+            The third row corresponds to the ymin+2*cell_size set of cells, etc
+        - Each row must have nx comma-separated entries.
+        - The entries are either 0 (cell is inactive) or 1 (cell is active)"""
+
+        try:
+            with open(filename, 'r') as f:
+                data = f.readlines()
+        except:
+            raise IOError('Cannot open or read ' + filename)
+
+        self.active = array.array('I', [])
+        checked_header = False
+        header_string_error = "The header line in " + filename + " does not match #xmin=" + str(self.xmin) + ",ymin=" + str(self.ymin) + ",cell_size=" + str(self.cell_size) + ",nx=" + str(self.nx) + ",ny=" + str(self.ny)
+        data_string_error = "Data in " + filename + " must be CSV formatted.  Each line in the file corresponds to a row (constant y) of cells, so must contain " + str(self.nx) + " entries (separated by commas).  Each entry must be either 0 (inactive) or 1 (active).  There must be " + str(self.ny) + " such rows.  The first row corresponds to cells at y=ymin, the next row at y=ymin+cell_size, etc"
+        for line in data:
+            if not line.strip():
+                continue
+            line = line.strip()
+            if line.startswith("#xmin"):
+                # catch the most common errors
+                line = line.split(",")
+                if len(line) != 5:
+                    raise ValueError(header_string_error)
+                if not (line[0].startswith("#xmin=") and line[1].startswith("ymin=") and line[2].startswith("cell_size=") and line[3].startswith("nx=") and line[4].startswith("ny=")):
+                    raise ValueError(header_string_error)
+                try:
+                    xmin = float(line[0].split("=")[1])
+                    ymin = float(line[1].split("=")[1])
+                    cell_size = float(line[2].split("=")[1])
+                    nx = int(line[3].split("=")[1])
+                    ny = int(line[4].split("=")[1])
+                except:
+                    raise ValueError(header_string_error)
+                if not (xmin == self.xmin and ymin == self.ymin and cell_size == self.cell_size and nx == int(self.nx) and ny == int(self.ny)):
+                    raise ValueError(header_string_error)
+                checked_header = True
+                continue
+            if line.startswith("#"):
+                continue
+            # can only get here if we should be reading data
+            if not checked_header:
+                raise ValueError(header_string_error)
+
+            try:
+                line = [int(i) for i in line.split(",")]
+            except:
+                raise ValueError(data_string_error)
+            if len(line) != self.nx:
+                raise ValueError(data_string_error)
+            array.extend(self.active, array.array('I', line))
+
+        if len(self.active) != self.num_cells:
+            raise ValueError(data_string_error)
+
+        # build active and adjacency information
+        self.computeNumActive()
+        self.buildAdjacency()
+
+
+    cdef void computeNumActive(self):
+        """Computes self.num_active_cells, self.global_index.  Also, checks if there are entries that are not zero or one"""
+
+        self.num_active_cells = 0
+        self.global_index = array.array('I', [])
+        self.active_index = array.array('I', [self.num_cells] * self.num_cells) # use some dummy values
+        cdef unsigned i
+        for i in range(len(self.active)):
+            if self.active[i] == 0:
+                continue
+            elif self.active[i] == 1:
+                array.extend(self.global_index, array.array('I', [i]))
+                self.active_index[i] = self.num_active_cells
+                self.num_active_cells += 1
+            else:
+                raise ValueError("Data in active matrix that determines whether a cell is active or inactive must be only 0 or 1, but yours includes the number " + str(self.active[i]))
+
+    cdef void buildAdjacency(self):
+        """Builds connect_from and connect_to, and num_connections, based on the self.active"""
 
         # build connectivity information
         self.connect_from = array.array('I', [])
@@ -56,39 +165,34 @@ cdef class Grid:
         # connecting to right neighbour
         for y_ind in range(self.ny):
             for x_ind in range(self.nx - 1):
-                ind = self.internal_index(x_ind, y_ind)
-                self.connect_from.extend(array.array('I', [ind]))
-                self.connect_to.extend(array.array('I', [ind + 1]))
+                ind = self.internal_global_index(x_ind, y_ind)
+                if (self.active[ind] == 1 and self.active[ind + 1] == 1):
+                    self.connect_from.extend(array.array('I', [self.active_index[ind]]))
+                    self.connect_to.extend(array.array('I', [self.active_index[ind + 1]]))
         # connecting to left neighbour
         for y_ind in range(self.ny):
             for x_ind in range(1, self.nx):
-                ind = self.internal_index(x_ind, y_ind)
-                self.connect_from.extend(array.array('I', [ind]))
-                self.connect_to.extend(array.array('I', [ind - 1]))
+                ind = self.internal_global_index(x_ind, y_ind)
+                if (self.active[ind] == 1 and self.active[ind - 1] == 1):
+                    self.connect_from.extend(array.array('I', [self.active_index[ind]]))
+                    self.connect_to.extend(array.array('I', [self.active_index[ind - 1]]))
         # connecting to bottom neighbour
         for y_ind in range(1, self.ny):
             for x_ind in range(self.nx):
-                ind = self.internal_index(x_ind, y_ind)
-                self.connect_from.extend(array.array('I', [ind]))
-                self.connect_to.extend(array.array('I', [ind - self.nx]))
+                ind = self.internal_global_index(x_ind, y_ind)
+                if (self.active[ind] == 1 and self.active[ind - self.nx] == 1):
+                    self.connect_from.extend(array.array('I', [self.active_index[ind]]))
+                    self.connect_to.extend(array.array('I', [self.active_index[ind - self.nx]]))
         # connecting to top neighbour
         for y_ind in range(self.ny - 1):
             for x_ind in range(self.nx):
-                ind = self.internal_index(x_ind, y_ind)
-                self.connect_from.extend(array.array('I', [ind]))
-                self.connect_to.extend(array.array('I', [ind + self.nx]))
+                ind = self.internal_global_index(x_ind, y_ind)
+                if (self.active[ind] == 1 and self.active[ind + self.nx] == 1):
+                    self.connect_from.extend(array.array('I', [self.active_index[ind]]))
+                    self.connect_to.extend(array.array('I', [self.active_index[ind + self.nx]]))
         self.num_connections = len(self.connect_from)
-            
 
-    cdef unsigned internal_index(self, unsigned x_ind, unsigned y_ind):
-        """provides the index used in this class, given x and y cell number.
-        Note that:
-        (x_ind, y_ind) = 0 corresponds to the lower left-hand cell, which is internal_index = 0
-        (x_ind, y_ind) = (nx - 1, 0) corresponds to the lower right-hand cell, which is internal_index = nx - 1
-        (x_ind, y_ind) = (0, ny - 1) corresponds to the upper left-hand cell, which is internal_index = (ny - 1) * nx
-        (x_ind, y_ind) = (nx - 1, ny - 1) corresponds to the upper right-hand cell, which is internal_index = (ny - 1) * nx + nx - 1
-        """
-        return y_ind * self.nx + x_ind
+
 
     cpdef float getXmin(self):
         "Returns x coord of the lower-left corner"""
@@ -126,4 +230,10 @@ cdef class Grid:
         """Returns the connections to the active cells"""
         return self.connect_to
 
+    cpdef array.array getGlobalIndex(self):
+        """Returns the global index of the active cells"""
+        return self.global_index
 
+    cpdef array.array getActiveIndex(self):
+        """Returns the active index of the the global cell array"""
+        return self.active_index
--- tests/TestGrid.py
+++ tests/TestGrid.py
@@ -21,6 +21,8 @@ class TestMe(unittest.TestCase):
 
    def setUp(self):
       self.g1 = Grid(1.0, 2.0, 3.0, 4, 3)
+      self.g2 = Grid(1.0, 2.0, 3.0, 4, 3)
+      self.g2.setActiveAndInactive(os.path.join(findbin, "inactive_active.csv"))
 
    def testNegativeCellSize(self):
       with self.assertRaises(ValueError) as the_err:
@@ -29,24 +31,39 @@ class TestMe(unittest.TestCase):
 
    def testGetXmin(self):
       self.assertEqual(self.g1.getXmin(), 1.0)
+      self.assertEqual(self.g2.getXmin(), 1.0)
 
    def testGetYmin(self):
       self.assertEqual(self.g1.getYmin(), 2.0)
+      self.assertEqual(self.g2.getYmin(), 2.0)
 
    def testGetNx(self):
       self.assertEqual(self.g1.getNx(), 4)
+      self.assertEqual(self.g2.getNx(), 4)
 
    def testGetNy(self):
       self.assertEqual(self.g1.getNy(), 3)
+      self.assertEqual(self.g2.getNy(), 3)
 
    def testGetNumCells(self):
       self.assertEqual(self.g1.getNumCells(), 12)
+      self.assertEqual(self.g2.getNumCells(), 12)
 
    def testGetNumActiveCells(self):
       self.assertEqual(self.g1.getNumActiveCells(), 12)
+      self.assertEqual(self.g2.getNumActiveCells(), 7)
 
    def testGetNumConnections(self):
       self.assertEqual(self.g1.getNumConnections(), 34)
+      self.assertEqual(self.g2.getNumConnections(), 10)
+
+   def testGetGlobalIndex(self):
+      self.assertTrue(arrayequal(self.g1.getGlobalIndex(), range(12)))
+      self.assertTrue(arrayequal(self.g2.getGlobalIndex(), [0, 2, 3, 4, 6, 10, 11]))
+
+   def testGetActiveIndex(self):
+      self.assertTrue(arrayequal(self.g1.getActiveIndex(), range(12)))
+      self.assertTrue(arrayequal(self.g2.getActiveIndex(), [0, 12, 1, 2, 3, 12, 4, 12, 12, 12, 5, 6]))
 
    def testConnections(self):
       f = self.g1.getConnectionsFrom()
@@ -65,6 +82,38 @@ class TestMe(unittest.TestCase):
       self.assertTrue(set([t[i] for i in r if f[i] == 9]) == set([8, 5, 10]))
       self.assertTrue(set([t[i] for i in r if f[i] == 10]) == set([9, 6, 11]))
       self.assertTrue(set([t[i] for i in r if f[i] == 11]) == set([10, 7]))
+
+
+      f = self.g2.getConnectionsFrom()
+      t = self.g2.getConnectionsTo()
+      r = range(len(f))
+
+      self.assertTrue(set([t[i] for i in r if f[i] == 0]) == set([3]))
+      self.assertTrue(set([t[i] for i in r if f[i] == 1]) == set([2, 4]))
+      self.assertTrue(set([t[i] for i in r if f[i] == 2]) == set([1]))
+      self.assertTrue(set([t[i] for i in r if f[i] == 3]) == set([0]))
+      self.assertTrue(set([t[i] for i in r if f[i] == 4]) == set([1, 5]))
+      self.assertTrue(set([t[i] for i in r if f[i] == 5]) == set([4, 6]))
+      self.assertTrue(set([t[i] for i in r if f[i] == 6]) == set([5]))
+
+
+   def testNoActiveFile(self):
+      with self.assertRaises(IOError) as the_err:
+         self.g1.setActiveAndInactive("no_such_file")
+      self.assertEqual(str(the_err.exception), "Cannot open or read no_such_file")
+
+   def testBadActiveFile(self):
+      for num in range(1, 13):
+         num = str(num)
+         with self.assertRaises(ValueError) as the_err:
+            self.g1.setActiveAndInactive(os.path.join(findbin, "bad_inactive_active" + num + ".csv"))
+         self.assertEqual(str(the_err.exception), "The header line in " + os.path.join(findbin, "bad_inactive_active" + num + ".csv") + " does not match #xmin=1.0,ymin=2.0,cell_size=3.0,nx=4,ny=3")
+
+      for num in range(13, 16):
+         num = str(num)
+         with self.assertRaises(ValueError) as the_err:
+            self.g1.setActiveAndInactive(os.path.join(findbin, "bad_inactive_active" + num + ".csv"))
+         self.assertEqual(str(the_err.exception), "Data in " + os.path.join(findbin, "bad_inactive_active" + num + ".csv") + " must be CSV formatted.  Each line in the file corresponds to a row (constant y) of cells, so must contain 4 entries (separated by commas).  Each entry must be either 0 (inactive) or 1 (active).  There must be 3 such rows.  The first row corresponds to cells at y=ymin, the next row at y=ymin+cell_size, etc")
 
 
 
