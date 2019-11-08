6365ef2ced0a8d13e6e6be4557cc5e47125af7ae
WilkAndy <andrew.wilkins@csiro.au>
2019-11-08 15:00:25 +1100

    assess_runtimes renamed.  Refs #18

--- example1/assess_diffusion_runtimes.py
+++ example1/assess_diffusion_runtimes.py
@@ -1,7 +1,6 @@
 # Processes wind vectors given by Nick into a single file, wind.csv, and changing to m/day
 import os
 import sys
-import array
 
 sys.stdout.write("NOTE: You will have to run this a few times to properly assess runtimes\n")
 sys.stdout.write("      This is because your runtime will be impacted by other processes on your system\n\n")
@@ -44,44 +43,18 @@ pop = list(range(all_pops.getCell().getNumberOfPopulations() + all_pops.getCell(
 all_pops.setPopulationAndParameters(g1.getNumActiveCells() // 2, pop)
 sys.stdout.write(" " + str(timeit.default_timer() - start) + "s\n")
 
-# initialise the spatial structure with timestep = 1 day and diffusion coefficient = 1E4 m^2/day and advection fraction = 0.5
-# No wind
 sys.stdout.write("Defining the spatial structure, ready for diffusion...")
 start = timeit.default_timer()
-spatial = SpatialDynamics(1.0, 1E4, 0.5, g1, array.array('f', []), [], all_pops)
+spatial = SpatialDynamics(g1, all_pops)
 sys.stdout.write(" " + str(timeit.default_timer() - start) + "s\n")
 
 sys.stdout.write("Diffusing\n")
 start = timeit.default_timer()
 for i in range(1, 11):
    sys.stdout.write("Time step " + str(i) + "\n")
-   spatial.diffuse()
+   # diffuse with timestep = 1 day and diffusion coefficient = 1E4 m^2/day
+   spatial.diffuse(1.0, 1E4)
 sys.stdout.write("Time for 1 diffusion step = " + str((timeit.default_timer() - start) / 10.0) + "s\n")
-
-if True:
-   sys.stdout.write("Initialising wind...")
-   start = timeit.default_timer()
-   w = Wind(os.path.join(findbin, "raw_wind.csv"), os.path.join(findbin, "wind_8_subdivisions.csv"), [[0.05, 0.125], [0.1, 0.125], [0.15, 0.125], [0.2, 0.125], [0.25, 0.125], [0.3, 0.125], [0.4, 0.125], [0.5, 0.125]], g1)
-   sys.stdout.write(" " + str(timeit.default_timer() - start) + "s\n")
-   particle_tracking_needed = True
-   if particle_tracking_needed:
-      sys.stdout.write("Parsing raw_wind.csv and doing particle tracking (not necessary if particle-tracking pre-done)...")
-      start = timeit.default_timer()
-      w.parseRawFile()
-      sys.stdout.write(" " + str(timeit.default_timer() - start) + "s\n")
-   output_required = False
-   if output_required:
-      sys.stdout.write("Outputting particle-tracking result (not necessary if particle-tracking pre-done)...")
-      start = timeit.default_timer()
-      w.outputProcessedCSV()
-      sys.stdout.write(" " + str(timeit.default_timer() - start) + "s\n")
-   input_required = False
-   if input_required:
-      sys.stdout.write("Inputing the particle-tracking result...")
-      start = timeit.default_timer()
-      w.parseProcessedFile()
-      sys.stdout.write(" " + str(timeit.default_timer() - start) + "s\n")
-
 
 
 sys.exit(0)
