6cc93beea396dbf558e4a1710b6f96a8118a70a9
nickbeeton <nick.beeton@utas.edu.au>
2020-10-14 15:07:49 +1100

    Update build_pearcey.sh
    With Ond's suggestions

--- code/build_pearcey.sh
+++ code/build_pearcey.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 module load openmpi/3.1.4-ofed45-gcc
+module load gcc
 module load git
 module load python/3.7.2
 
 cd auxillary; ./build_pearcey.sh ; cd ..
 
 # get the numpy include directory using import numpy then numpy.get_include()
-gcc_flags="-shared -fno-strict-aliasing -Wsign-compare -Wunreachable-code -DNDEBUG -g -O3 -Wall -I/apps/python/3.7.2/include/python3.7m -I/apps/python/3.7.2/lib/python3.7/site-packages/numpy-1.15.4-py3.7-linux-x86_64.egg/numpy/core/include -fPIC"
+gcc_flags="-shared -fno-strict-aliasing -Wsign-compare -Wunreachable-code -DNDEBUG -g -O3 -Wall -I/apps/python/3.7.2/include/python3.7m -I/apps/python/3.7.2/lib/python3.7/site-packages/numpy-1.15.4-py3.7-linux-x86_64.egg/numpy/core/include -fPIC -march=native"
 
 mpicc $gcc_flags csvparser.c -o csvparser.so
 
