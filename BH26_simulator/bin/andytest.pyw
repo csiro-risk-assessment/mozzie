c71adbf30be1bd13524075d7542bec0f280adf57
WilkAndy <andrew.wilkins@csiro.au>
2022-03-03 20:18:04 +1100

    BH26_simulator added.  It works on mac after cythonising.  TODO: get it to work on windows.   TODO: create a windows distributable

---
+++ BH26_simulator/bin/andytest.pyw
@@ -0,0 +1,228 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import pathlib
+import appdirs
+import configparser
+
+from PyQt5.QtWidgets import (
+    QApplication, QFormLayout, QWidget, QLineEdit,
+    QMainWindow, QMenu, QAction, QVBoxLayout, QDialog,
+    QDialogButtonBox, QTextEdit, QMessageBox, QFileDialog, QPushButton,
+)
+from PyQt5.QtCore import Qt, pyqtSignal
+from PyQt5.QtGui import QIcon
+
+from simulator import Simulator
+
+
+class ClickableLineEdit(QLineEdit):
+    clicked = pyqtSignal()
+
+    def mousePressEvent(self, e):
+        if e.button() == Qt.LeftButton:
+            self.clicked.emit()
+        else:
+            super().mousePressEvent(e)
+
+
+class Window(QMainWindow):
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle("Mosquito lifecycle")
+
+        # icon
+        if getattr(sys, 'frozen', False):
+            fname = pathlib.Path(sys._MEIPASS) / 'andytest.ico'
+        else:
+            me = pathlib.Path(sys.argv[0]).resolve()
+            fname = me.parent.parent / 'etc' / 'andytest.ico'
+        # on linux we need a ppm/pgm file, not a windows ico, just ignore
+        try:
+            self.setWindowIcon(QIcon(str(fname)))
+        except Exception:
+            pass
+
+        # vlayout contains the form layout with Go button at the bottom
+        vlayout = QVBoxLayout()
+        flayout = QFormLayout()
+
+        self.in_params = in_params = ClickableLineEdit()
+        in_params.setToolTip('CSV file quantifying the parameters')
+        flayout.addRow("Parameters file:", in_params)
+        in_params.clicked.connect(self.set_in_params)
+
+        self.out_fn = out_fn = ClickableLineEdit()
+        out_fn.setToolTip('Output will be written to this file')
+        flayout.addRow("Output file:", out_fn)
+        out_fn.clicked.connect(self.set_out_fn)
+
+        vlayout.addLayout(flayout)
+
+        self.but = QPushButton('GO', self)
+        self.but.setToolTip('Run mosquito lifecycle')
+        self.but.setStyleSheet('color: green')
+        self.but.clicked.connect(self.go)
+        vlayout.addWidget(self.but)
+
+        widget = QWidget()
+        widget.setLayout(vlayout)
+
+        # config file
+        adirs = appdirs.AppDirs('Mosquito lifecycle', 'CSIRO')
+        self.cdir = cdir = pathlib.Path(adirs.user_data_dir)
+        os.makedirs(cdir, mode=0o755, exist_ok=True)
+        self.cfile = pathlib.Path(adirs.user_data_dir) / 'config.ini'
+        print(self.cfile)
+        self.__load_config()
+
+        self.__create_menus()
+
+        self.setCentralWidget(widget)
+
+    def __create_menus(self):
+        bar = self.menuBar()
+        self.setMenuBar(bar)
+
+        menu = QMenu("&File", self)
+        bar.addMenu(menu)
+        action = QAction("&Quit", self)
+        action.triggered.connect(self.close)
+        menu.addAction(action)
+
+        menu = QMenu("&Help", self)
+        bar.addMenu(menu)
+        action = QAction("&About", self)
+        action.triggered.connect(lambda: self.msg('About', 'version.txt'))
+        menu.addAction(action)
+        action = QAction("&Help", self)
+        action.triggered.connect(lambda: self.msg('Help', 'help.html'))
+        menu.addAction(action)
+
+    def __load_config(self):
+        if not self.cfile.exists():
+            cp = configparser.ConfigParser()
+            cp['DEFAULT'] = {
+                'in_dir': pathlib.Path.home() / 'Downloads',
+                'out_dir': pathlib.Path.home() / 'Downloads',
+            }
+            with open(self.cfile, 'w') as cfh:
+                cp.write(cfh)
+
+        self.cp = configparser.ConfigParser()
+        self.cp.read(self.cfile)
+
+    def save_config(self):
+        with open(self.cfile, 'w') as cfh:
+            self.cp.write(cfh)
+
+    def msg(self, title, fname):
+
+        class ScrollableDialog(QDialog):
+
+            def __init__(self, title, txt, parent=None, html=False):
+                super().__init__(parent)
+                self.setWindowTitle(title)
+                btns = QDialogButtonBox(QDialogButtonBox.Ok)
+                btns.accepted.connect(self.accept)
+                layout = QVBoxLayout()
+                te = QTextEdit()
+                if html:
+                    te.setHtml(txt)
+                else:
+                    te.setText(txt)
+                te.setMinimumWidth(600)
+                te.setMinimumHeight(400)
+                te.setReadOnly(True)
+                layout.addWidget(te)
+                layout.addWidget(btns)
+                self.setLayout(layout)
+
+        if getattr(sys, 'frozen', False):
+            fname = pathlib.Path(sys._MEIPASS) / fname
+        else:
+            me = pathlib.Path(sys.argv[0]).resolve()
+            fname = me.parent / fname
+
+        with open(fname) as fh:
+            txt = fh.read()
+            # if txt is short a messagebox will do
+            if txt.count('\n') < 5:
+                QMessageBox.information(self, 'About', txt)
+            else:
+                dl = ScrollableDialog(
+                    title, txt, parent=self,
+                    html=pathlib.Path(fname).suffix == '.html'
+                )
+                dl.exec()
+
+    def set_in_params(self):
+        fname, check = QFileDialog.getOpenFileName(
+            self, "CSV input parameters", self.cp['DEFAULT']['in_dir'],
+            "CSV File (*.csv)",
+        )
+        if not check:
+            return
+
+        # make sure extension is .csv
+        if pathlib.Path(fname).suffix != '.csv':
+            fname += '.csv'
+
+        # so we use the same directory next time
+        self.cp['DEFAULT']['in_dir'] = str(pathlib.Path(fname).parent)
+        self.save_config()
+
+        self.in_params.setText(fname)
+
+    def set_out_fn(self):
+        fname, check = QFileDialog.getOpenFileName(
+            self, "Output file", self.cp['DEFAULT']['out_dir'],
+            "CSV File (*.csv)",
+        )
+        if not check:
+            return
+
+        # make sure extension is .csv
+        if pathlib.Path(fname).suffix != '.csv':
+            fname += '.csv'
+
+        # so we use the same directory next time
+        self.cp['DEFAULT']['out_dir'] = str(pathlib.Path(fname).parent)
+        self.save_config()
+
+        self.out_fn.setText(fname)
+
+    def go(self):
+        in_params = self.in_params.text()
+        out_fn = self.out_fn.text()
+
+        #fibo = routines.fib(10)
+        #QMessageBox.information(self, 'Fibo', ' '.join(map(str, fibo)))
+        #return 
+
+        if not in_params:
+            QMessageBox.critical(
+                self, "Error", "Provide a CSV file quantifying the parameters"
+            )
+            return False
+        if not out_fn:
+            QMessageBox.critical(
+                self, "Error", "Must provide an output filename"
+            )
+            return False
+
+        simulator = Simulator(in_params)
+        simulator.go()
+        simulator.output(out_fn)
+        
+
+
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    win = Window()
+    win.show()
+    sys.exit(app.exec_())
---
+++ BH26_simulator/bin/simulator.py
@@ -0,0 +1,270 @@
+import os
+import sys
+import array
+
+sys.path.append("../../code")
+from cellDynamics import CellDynamicsMosquitoBH26Delay
+
+class Simulator:
+
+    def __init__(self, fn):
+        with open(fn, 'r') as f:
+            data = f.readlines()
+        self.values = {}
+        for key in ["num_species", "delay", "death_rate", "competition", "emergence_rate", "activity", "reduction", "hybridisation", "sex_ratio", "female_bias", "m_w", "m_c", "small_value", "initial_populations", "qm", "day_of_introduction", "species_number_of_introduction", "genotype_of_introduction", "sex_of_introduction", "quantity_of_introduction", "simulation_days"]:
+            self.values[key] = None
+        
+        for line in data:
+            if not line.strip(): continue
+            line = line.strip()
+            if line.startswith("#"): continue
+            key, line = line.split(",", 1)
+            key = key.strip()
+
+            if key == "num_species" or key == "delay" or key == "day_of_introduction" or key == "simulation_days" or key == "quantity_of_introduction":
+                self.values[key] = int(line)
+            elif key == "species_number_of_introduction":
+                self.values[key] = int(line) - 1
+            elif key == "sex_of_introduction":
+                line = line.strip().lower()
+                if line == "male":
+                    self.values[key] = 0
+                elif line == "female":
+                    self.values[key] = 1
+                else:
+                    sys.stderr.write("Error: sex_of_introduction must be either 'Male' or 'Female'\n")
+                    sys.exit(1)
+            elif key == "genotype_of_introduction":
+                line = line.strip().lower()
+                if line == "ww":
+                    self.values[key] = 0
+                elif line == "wc":
+                    self.values[key] = 1
+                elif line == "wr":
+                    self.values[key] = 2
+                elif line == "cc":
+                    self.values[key] = 3
+                elif line == "cr":
+                    self.values[key] = 4
+                elif line == "cc":
+                    self.values[key] = 5
+                else:
+                    sys.stderr.write("Error: genotype_of_introduction must be ww, wc, wr, cc, cr or cc\n")
+                    sys.exit(1)
+            elif key == "death_rate":
+                s = list(map(float, line.split(",")))
+                self.values["death_rate"] = [s[i::6] for i in range(6)]
+            elif key == "competition":
+                s = list(map(float, line.split(",")))
+                self.values["competition"] = [s[i::3] for i in range(3)]
+            elif key == "emergence_rate":
+                self.values["emergence_rate"] = list(map(float, line.split(",")))
+            elif key == "activity":
+                s = list(map(float, line.split(",")))
+                self.values["activity"] = [s[i::3] for i in range(3)]
+            elif key == "reduction":
+                s = list(map(float, line.split(",")))
+                self.values["reduction"] = [s[i::6] for i in range(6)]
+            elif key == "hybridisation":
+                s = list(map(float, line.split(",")))
+                s = [s[i::3] for i in range(3)]
+                self.values["hybridisation"] = [[x[i::3] for i in range(3)] for x in s]
+            elif key == "death_rate" or key == "sex_ratio" or key == "female_bias" or key == "m_w" or key == "m_c" or key == "small_value":
+                self.values[key] = float(line)
+            elif key == "initial_populations" or key == "qm":
+                self.values[key] = list(map(float, line.split(",")))
+        self.cell = CellDynamicsMosquitoBH26Delay(num_species = self.values["num_species"],
+                                                  delay = self.values["delay"],
+                                                  current_index = 0,
+                                                  death_rate = self.values["death_rate"],
+                                                  competition = self.values["competition"],
+                                                  emergence_rate = self.values["emergence_rate"],
+                                                  activity = self.values["activity"],
+                                                  reduction = self.values["reduction"],
+                                                  hybridisation = self.values["hybridisation"],
+                                                  sex_ratio = self.values["sex_ratio"],
+                                                  female_bias = self.values["female_bias"],
+                                                  m_w = self.values["m_w"],
+                                                  m_c = self.values["m_c"])
+        self.cell.setSmallValue(self.values["small_value"])
+
+        self.populations = []
+        
+
+    def getValues(self):
+        return self.values
+
+    def getPopulations(self):
+        return self.populations
+
+    def getCurrentAdults(self, pops_and_params):
+        num_species = self.values["num_species"]
+        num_genotypes = 6
+        num_sexes = 2
+        current_adults = [0 for i in range(num_species * num_genotypes * num_sexes)]
+        for sex in range(num_sexes):
+            for genotype in range(num_genotypes):
+                for species in range(num_species):
+                    for d in [self.cell.getCurrentIndex()]:
+                        indi = species + genotype * num_species + sex * num_species * num_genotypes + d * num_species * num_genotypes * num_sexes
+                        indj = species + genotype * num_species + sex * num_species * num_genotypes
+                        current_adults[indj] = pops_and_params[indi]
+        return current_adults
+
+
+    def go(self):
+        num_pops = self.cell.getNumberOfPopulations()
+        num_species = self.values["num_species"]
+        num_genotypes = 6
+        num_sexes = 2
+        delay = self.cell.getDelay()
+
+        # initialise adult populations
+        initial_condition = [0 for i in range(num_pops)]
+        for sex in range(num_sexes):
+            for genotype in range(1): # only ww initially
+                for species in range(num_species):
+                    for d in range(delay + 1):
+                        indi = species + genotype * num_species + sex * num_species * num_genotypes + d * num_species * num_genotypes * num_sexes
+                        indj = sex + species * num_sexes
+                        initial_condition[indi] = self.values["initial_populations"][indj]
+        initial_condition += self.values["qm"]
+        pap = array.array('f', initial_condition)
+
+        self.populations = []
+
+        # burn-in
+        for timestep in range(self.values["day_of_introduction"]):
+            self.populations.append(self.getCurrentAdults(pap))
+            self.cell.evolve(1.0, pap)
+            self.cell.incrementCurrentIndex()
+
+        # introduce
+        indi = self.values["species_number_of_introduction"] + self.values["genotype_of_introduction"] * num_species + self.values["sex_of_introduction"] * num_species * num_genotypes + self.cell.getCurrentIndex() * num_species * num_genotypes * num_sexes
+        pap[indi] += self.values["quantity_of_introduction"]
+
+        # evolve
+        for timestep in range(self.values["day_of_introduction"], self.values["simulation_days"]):
+            self.populations.append(self.getCurrentAdults(pap))
+            self.cell.evolve(1.0, pap)
+            self.cell.incrementCurrentIndex()
+
+        
+    def genotype_string(self, genotype_num):
+        if genotype_num == 0:
+            return "ww"
+        elif genotype_num == 1:
+            return "wc"
+        elif genotype_num == 2:
+            return "wr"
+        elif genotype_num == 3:
+            return "cc"
+        elif genotype_num == 4:
+            return "cr"
+        elif genotype_num == 5:
+            return "rr"
+        return 'None'
+        
+    def output(self, fn):
+        num_species = self.values["num_species"]
+        with open(fn, 'w') as f:
+            f.write("# Output produced by BH26Delay simulator with the following parameters\n")
+            f.write("num_species, " + str(num_species) + "\n")
+            f.write("delay, " + str(self.cell.getDelay()) + "\n")
+
+            comment = "# Death rates"
+            vals = "death_rate"
+            dr = self.cell.getDeathRate()
+            for sp in range(num_species):
+                for g in range(6):
+                    comment += ", " + self.genotype_string(g) + "_" + str(sp + 1)
+                    vals += ", " + str(dr[g][sp])
+            f.write(comment + "\n")
+            f.write(vals + "\n")
+
+            comment = "# Competition"
+            vals = "competition"
+            c = self.cell.getCompetition()
+            for s1 in range(num_species):
+                for s2 in range(num_species):
+                    comment += ", " + str(s1 + 1) + "v" + str(s2 + 1)
+                    vals += ", " + str(c[s2][s1])
+            f.write(comment + "\n")
+            f.write(vals + "\n")
+            
+            comment = "# Emergence rate"
+            vals = "emergence_rate"
+            c = self.cell.getEmergenceRate()
+            for s1 in range(num_species):
+                comment += ", " + str(s1 + 1)
+                vals += ", " + str(c[s1])
+            f.write(comment + "\n")
+            f.write(vals + "\n")
+
+            comment = "# activity between species F=Female M=Male"
+            vals = "activity"
+            c = self.cell.getActivity()
+            for sM in range(num_species):
+                for sF in range(num_species):
+                    comment += ", F" + str(sF + 1) + "_M" + str(sM + 1)
+                    vals += ", " + str(c[sF][sM])
+            f.write(comment + "\n")
+            f.write(vals + "\n")
+
+            comment = "# reduction due to construct F=Female M=Male"
+            vals = "reduction"
+            c = self.cell.getReduction()
+            for gF in range(6):
+                for gM in range(6):
+                    comment += ", " + self.genotype_string(gM) + "M_" + self.genotype_string(gF) + "F"
+                    vals += ", " + str(c[gM][gF])
+            f.write(comment + "\n")
+            f.write(vals + "\n")
+
+            comment = "# hybridisation F=Female M=Male o=offspring"
+            vals = "hybridisation"
+            c = self.cell.getHybridisation()
+            for so in range(num_species):
+                for sF in range(num_species):
+                    for sM in range(num_species):
+                        comment += ", M" + str(sM) + "_F" + str(sF) + "_o" + str(so)
+                        vals += ", " + str(c[sM][sF][so])
+            f.write(comment + "\n")
+            f.write(vals + "\n")
+
+            f.write("sex_ratio, " + str(self.cell.getSexRatio()) + "\n")
+            f.write("female_bias, " + str(self.cell.getFemaleBias()) + "\n")
+            f.write("m_w, " + str(self.values["m_w"]) + "\n")
+            f.write("m_c, " + str(self.values["m_c"]) + "\n")
+            f.write("small_value, " + str(self.cell.getSmallValue()) + "\n")
+
+            f.write("# initial wild-types F=Female M=Male, M1, F1, M2, F2, M3, F3 ...\n")
+            f.write("initial_populations, " + ",".join(map(str, self.values["initial_populations"])) + "\n")
+            f.write("# qm values (related to carrying capacity), QM1, QM2, QM3 ...\n")
+            f.write("qm, " + ",".join(map(str, self.values["qm"])) + "\n")
+            f.write("day_of_introduction, " + str(self.values["day_of_introduction"]) + "\n")
+            f.write("genotype_of_introduction, " + self.genotype_string(self.values["genotype_of_introduction"]) + "\n")
+            f.write("species_number_of_introduction, " + str(self.values["species_number_of_introduction"] + 1) + "\n")
+            f.write("sex_of_introduction, " + ("Male" if (self.values["sex_of_introduction"] == 0) else "Female") + "\n")
+            f.write("quantity_of_introduction, " + str(self.values["quantity_of_introduction"]) + "\n")
+            f.write("simulation_days, " + str(self.values["simulation_days"]) + "\n")
+
+            f.write("#\n# Results.  First number is species.  Next letters indicate genotype.  Final is Male or Female\n")
+            header = "day "
+            for species in range(num_species):
+                for genotype in range(6):
+                    for sex in ['M', 'F']:
+                        header += ", " + str(species + 1) + self.genotype_string(genotype) + sex
+            f.write(header + "\n")
+            t = 0
+            for p in self.populations:
+                result = str(t) 
+                for species in range(num_species):
+                    for genotype in range(6):
+                        for sex in range(2):
+                            indj = species + genotype * num_species + sex * num_species * 6
+                            result += ", " + str(p[indj])
+                f.write(result + "\n")
+                t += 1
+                            
+            
