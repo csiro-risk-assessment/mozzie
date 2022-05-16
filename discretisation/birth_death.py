91e8b04551d34fad5202e62d9bfa3f00afc67271
WilkAndy <andrew.wilkins@csiro.au>
2022-05-16 15:17:27 +1000

    birth_death bug fix

--- discretisation/birth_death.py
+++ discretisation/birth_death.py
@@ -5,7 +5,7 @@ import numpy as np
 print("Solves dx(t)/dt = -death * x(t) + birth * x(t - lag) / (1 + x(t - lag) / qm)")
 print("Using:")
 print(" - Explicit Euler: x(t + dt) = x(t) + dt * (-death * x(t) + birth * x(t - lag) / (1 + x(t - lag) / qm)")
-print(" - Andy: x(t + dt) = r(t) + (x(t) - r) * exp(-dt / death), where r(t) = birth * x(t - lag) / death / (1 + x(t - lag) / qm)")
+print(" - Andy: x(t + dt) = r(t) + (x(t) - r) * exp(-dt * death), where r(t) = birth * x(t - lag) / death / (1 + x(t - lag) / qm)")
 ###  Make these floats, not integers!
 death = 1.0 # 1/days
 birth = 2.0 # 1/days
@@ -32,7 +32,7 @@ def andy(x, t_plus_dt):
     t_index = np.argmin(np.abs(times - (t_plus_dt - dt)))
     lagged_index = np.argmin(np.abs(times - (t_plus_dt - dt - lag)))
     r = birth * x[lagged_index] / death / (1 + x[lagged_index] / qm)
-    return (t_plus_dt_index, r + (x[t_index] - r) * np.exp(-dt / death))
+    return (t_plus_dt_index, r + (x[t_index] - r) * np.exp(-dt * death))
 
 for t in np.arange(dt, end_time, dt):
     ind, expl = euler(x_euler, t)
