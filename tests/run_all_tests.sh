67a2e3f0aa2846b0c89b191993f70b863c201f3a
WilkAndy <andrew.wilkins@csiro.au>
2024-03-07 10:00:09 +1000

    rename the Test python files to test, so python -m unittest works

--- tests/run_all_tests.sh
+++ tests/run_all_tests.sh
@@ -2,26 +2,26 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-python3 ${DIR}/TestGrid.py
-python3 ${DIR}/TestSpatialDynamics.py
-python3 ${DIR}/TestCellDynamicsStatic15_9_3_2.py
-python3 ${DIR}/TestCellDynamicsLogistic1_1.py
-python3 ${DIR}/TestCellDynamicsBeeton2_2.py
-python3 ${DIR}/TestCellDynamicsMosquito23.py
-python3 ${DIR}/TestPopulationsAndParameters.py
-python3 ${DIR}/TestWind.py
-python3 ${DIR}/TestDiffusion_1.py
-python3 ${DIR}/TestDiffusion_2.py
-python3 ${DIR}/TestAdvection_1.py
-python3 ${DIR}/TestAdvection_2.py
-python3 ${DIR}/TestAdvection_3.py
-python3 ${DIR}/TestEvolveCells.py
-python3 ${DIR}/TestCellDynamicsMosquito26.py
-python3 ${DIR}/TestCellDynamicsDelayBase.py
-python3 ${DIR}/TestCellDynamics26DelayBase.py
-python3 ${DIR}/TestCellDynamicsMosquitoLogistic26Delay.py
-python3 ${DIR}/TestCellDynamicsMosquitoBH26Delay.py
-python3 ${DIR}/TestCalcQm.py
+python3 ${DIR}/testGrid.py
+python3 ${DIR}/testSpatialDynamics.py
+python3 ${DIR}/testCellDynamicsStatic15_9_3_2.py
+python3 ${DIR}/testCellDynamicsLogistic1_1.py
+python3 ${DIR}/testCellDynamicsBeeton2_2.py
+python3 ${DIR}/testCellDynamicsMosquito23.py
+python3 ${DIR}/testPopulationsAndParameters.py
+python3 ${DIR}/testWind.py
+python3 ${DIR}/testDiffusion_1.py
+python3 ${DIR}/testDiffusion_2.py
+python3 ${DIR}/testAdvection_1.py
+python3 ${DIR}/testAdvection_2.py
+python3 ${DIR}/testAdvection_3.py
+python3 ${DIR}/testEvolveCells.py
+python3 ${DIR}/testCellDynamicsMosquito26.py
+python3 ${DIR}/testCellDynamicsDelayBase.py
+python3 ${DIR}/testCellDynamics26DelayBase.py
+python3 ${DIR}/testCellDynamicsMosquitoLogistic26Delay.py
+python3 ${DIR}/testCellDynamicsMosquitoBH26Delay.py
+python3 ${DIR}/testCalcQm.py
 
 
 
